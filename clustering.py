5800a10f41c9d468a5abcf84e83ec9bf683095e2
Kazukifujii <fuziikazuki@gmail.com>
2022-08-30 17:53:50 +0900

    add 'clustering.py'

--- clustering.py
+++ clustering.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import linkage, dendrogram
 import os 
@@ -6,9 +7,10 @@ import sys
 import pandas as pd
 cwd=os.getcwd()
 
-dir=cwd+'/crystal_emd/result/cod/ABW/'
+dir=cwd+'/result/cod/ABW/'
+
 os.chdir(dir)
-print(os.getcwd())
+
 selfcsv=glob.glob('*self_distance.csv')[0]
 
 df=pd.read_csv(selfcsv,index_col=0)
@@ -20,12 +22,10 @@ for _,data in df.iterrows():
 
 clusteringdf=pd.DataFrame(machingid,columns=['id','distance']).set_index('id')
 
-result1 = linkage(clusteringdf, method = 'weighted')
-dendrogram(result1)
-print(result1)
-
-sys.exit()
+result1 = linkage(clusteringdf, method = 'single')
+dendrogram(result1,p=20,truncate_mode='lastp',labels=clusteringdf.index.to_list())
 
 plt.title("ABW_self_distance")
 plt.ylabel("Threshold")
 plt.show()
+os.chdir(cwd)
