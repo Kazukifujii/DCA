4f63523984998aefd3dec34e29279f94c1574629
Kazukifujii <fuziikazuki@gmail.com>
2022-09-06 21:32:36 +0900

    add 'squareform'function in calutering.py

--- clustering.py
+++ clustering.py
@@ -1,18 +1,18 @@
-from cmath import isclose, nan
-import copy,os,glob,sys
+import copy,os,glob
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import linkage, dendrogram
 import pandas as pd
 import math
-from scipy.cluster.hierarchy import ClusterWarning
-from warnings import simplefilter
+import copy
+from scipy.spatial.distance import squareform
 
-def make_sort_distance(selfcsv,outfil='sort_self_distanc.csv'):
+def make_sort_distance(selfcsv,csvn='sort_self_distanc.csv'):
     df=pd.read_csv(selfcsv,index_col=['isite_i','isite_j','pattern_i','pattern_j'])
     sortindex=list()
     for i,data in df.groupby(level=[0,1]):
         sortindex.append(data.sort_values('distance').iloc[0,0])
     matrixdf=pd.DataFrame()
+    standdf_=list()
     for index in sortindex:
         isite_i,isite_j,_,_=df[df.iloc[:,0]==index].index.to_list()[0]
         distance=df[df.iloc[:,0]==index].distance.values[0]
@@ -20,23 +20,24 @@ def make_sort_distance(selfcsv,outfil='sort_self_distanc.csv'):
             distance=0.0
         matrixdf.at[isite_i,isite_j]=copy.deepcopy(distance)
         matrixdf.at[isite_j,isite_i]=copy.deepcopy(distance)
+        standdf_.append((isite_i,isite_j,copy.deepcopy(distance)))
     matrixdf=matrixdf.fillna(0)
     matrixdf=matrixdf.sort_index()
     matrixdf=matrixdf.sort_index(axis=1)
-    matrixdf.to_csv(outfil)
+    standdf=pd.DataFrame(standdf_,columns=['isite_i','isite_j','distance'])
+    standdf.to_csv(csvn)
     return matrixdf
 
-def make_self_clusering(dir,outfil='sort_self_distanc.csv'):
+def make_self_clusering(dir,csvn='sort_self_distanc.csv',pngn='cluster.png',method='ward'):
     cwd=os.getcwd()
     os.chdir(dir)
     selfcsv=glob.glob('*self_distance.csv')[0]
-    cifid=os.path.basename(dir)
-    simplefilter("ignore", ClusterWarning)
-    matrixdf=make_sort_distance(selfcsv,outfil)
-    result1 = linkage(matrixdf, method = 'ward')
-    dendrogram(result1)
-    plt.title("{}_self_clutering".format(cifid))
-    plt.savefig("{}_self_clutering".format(cifid))
+    matrixdf=make_sort_distance(selfcsv,csvn)
+    result1 = linkage(squareform(matrixdf), method = method)
+    dendrogram(result1,labels=matrixdf.index.to_list())
+    plt.title(pngn)
+    plt.savefig(pngn)
+    plt.close()
     os.chdir(cwd)
 
 
@@ -48,4 +49,4 @@ import re
 for i in cifdir:
     cifid=re.split('/',i)[-1]
     print(cifid)
-    make_self_clusering(i,outfil='{}_sort_self_distanc.csv'.format(cifid))
+    make_self_clusering(i,csvn='{}_sort_self_distanc.csv'.format(cifid),pngn='{}_self_cluster.png'.format(cifid))
--- testmethod.py
+++ testmethod.py
@@ -1,21 +1,54 @@
-from turtle import fd
-from distance_func import make_distance
-import re
-dir1='result/sorttest/AEI/AEI_15_0.csv'
-dir2='result/sorttest/AEI/AEI_2_1.csv'
-dir='result/sorttest/AEI'
-
-import glob
+import copy,os,glob
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import linkage, dendrogram
+import pandas as pd
+import math
+from scipy.cluster.hierarchy import ClusterWarning
+from warnings import simplefilter
 import copy
-dir11=glob.glob(dir+'/AEI_15_*csv')
-dir22=glob.glob(dir+'/AEI_2_*csv')
-fdis=100
-for d1 in dir11:
-    print(re.split('_',d1)[-1].replace('.csv',''),re.split('_',d)[-1].replace('.csv',''))
-    dis=make_distance(d1,d)
-    if fdis>=dis:
-        
-        fdis=copy.deepcopy(dis)
-    print(dis)
-print()
-print(fdis)
+
+def make_sort_distance(selfcsv,csvn='sort_self_distanc.csv'):
+    df=pd.read_csv(selfcsv,index_col=['isite_i','isite_j','pattern_i','pattern_j'])
+    sortindex=list()
+    for i,data in df.groupby(level=[0,1]):
+        sortindex.append(data.sort_values('distance').iloc[0,0])
+    matrixdf=pd.DataFrame()
+    standdf_=list()
+    for index in sortindex:
+        isite_i,isite_j,_,_=df[df.iloc[:,0]==index].index.to_list()[0]
+        distance=df[df.iloc[:,0]==index].distance.values[0]
+        if math.isclose(distance,0.0,abs_tol=0.05):
+            distance=0.0
+        matrixdf.at[isite_i,isite_j]=copy.deepcopy(distance)
+        matrixdf.at[isite_j,isite_i]=copy.deepcopy(distance)
+        standdf_.append((isite_i,isite_j,copy.deepcopy(distance)))
+    matrixdf=matrixdf.fillna(0)
+    matrixdf=matrixdf.sort_index()
+    matrixdf=matrixdf.sort_index(axis=1)
+    standdf=pd.DataFrame(standdf_,columns=['isite_i','isite_j','distance'])
+    standdf.to_csv(csvn)
+    return matrixdf
+
+def make_self_clusering(dir,csvn='sort_self_distanc.csv',pngn='cluster.png',method='ward'):
+    cwd=os.getcwd()
+    os.chdir(dir)
+    selfcsv=glob.glob('*self_distance.csv')[0]
+    matrixdf=make_sort_distance(selfcsv,csvn)
+    from scipy.spatial.distance import squareform
+    result1 = linkage(squareform(matrixdf), method = method)
+    dendrogram(result1,labels=matrixdf.index.to_list())
+    plt.title(pngn)
+    plt.savefig(pngn)
+    plt.close()
+    os.chdir(cwd)
+
+
+import subprocess
+cifdir_ = subprocess.getoutput("find {0} -type d | sort".format('result/sorttest'))
+cifdir = cifdir_.split('\n')
+del cifdir[0]
+import re
+for i in cifdir:
+    cifid=re.split('/',i)[-1]
+    print(cifid)
+    make_self_clusering(i,csvn='{}_sort_self_distanc.csv'.format(cifid),pngn='{}_self_cluster.png'.format(cifid))
