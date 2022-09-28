a8b8fcf3d12aaea5d170c626cc1caf3e9bb8de5a
Kazukifujii <fuziikazuki@gmail.com>
2022-09-28 15:56:13 +0900

    coment

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ from crystal_emd.read_info import remake_csv
 
 
 adress='result/testcif/ABW/ABW_0_0.csv'''
-#出力するファイルの名前とディレクトリは変更可能
-#outname='test.csv'
+#出力するファイルの名前とディレクトリは変更可能：remake_csv(adress,outname='test.csv')
+
+#outnameを指定しないとcsvファイルが存在するディレクトリにSi_ABW_0_0.csvという名前で出力される
 remake_csv(adress)
