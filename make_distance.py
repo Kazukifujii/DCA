8a74d9dc946700becc1fec967e4f1e7df2b866f0
Kazukifujii <fuziikazuki@gmail.com>
2022-10-31 18:01:44 +0900

    10/31

--- make_distance.py
+++ make_distance.py
@@ -7,7 +7,7 @@ dir='result/testcif'
 cifdir=pd.read_csv('{}/picupadress'.format(dir),index_col=0).cifadress.to_list()
 cwd = os.getcwd()
 errorid=list()
-
+"""
 for i in cifdir:
     cifid=os.path.basename(i)
     print(cifid)
@@ -17,7 +17,8 @@ for i in cifdir:
     flusterdf.to_csv("{}/{}_fcluster.csv".format(i,cifid))
 
 fclusterdf=fcluster_list(dir)
+"""
 
 make_distance_csv(listadress=fclusterdf,resultname ="all_distance.csv")
-alldistance=pd.read_csv("all_distance.csv",index_col=0)
-make_clusering(csvadress="matrix_all_sort_distance.csv",csvn="all_sort_distance.csv",pngn="all_cluster.png",cal_matrixdf=False)
+#alldistance=pd.read_csv("all_distance.csv",index_col=0)
+make_clusering(csvadress="all_distance.csv",pngn="all_cluster.svg")
