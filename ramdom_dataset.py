73cc66379d024ea8a361134eb66fac3f47d352bf
Kazukifujii <fuziikazuki@gmail.com>
2022-11-10 21:47:52 +0900

    cheak program at test2.py

--- ramdom_dataset.py
+++ ramdom_dataset.py
@@ -1,11 +1,14 @@
 from glob import glob as gl
 import random,sys
+import shutil,os
 from crystal_emd.cluster_pointing import make_cluster_point
 datasetadress='result/allzeorite'
 import pandas as pd
 d=pd.read_csv('result/allzeorite/picupadress',index_col=0)
+datasetdir='cluster_dataset'
+if os.path.isdir(datasetdir):
+        shutil.rmtree(datasetdir)
 
-import shutil,os
 for sampleadress in d.sample(100,random_state=0).cifadress.to_list():
     copyadress='cluster_dataset/{}'.format(os.path.basename(sampleadress))
     if os.path.isdir(copyadress):
--- test2.py
+++ test2.py
@@ -5,20 +5,15 @@ d=make_crystall_point('cluster_dataset')
 import pandas as pd
 resulttxt='result/allzeorite/cifpoint'
 text_file=open(resulttxt,'w')
-text_file.write('cifid,point')
+text_file.write('cifid,point\n')
 text_file.close()
-import sys
-text_file=open(resulttxt,'w')
-cifid,cluster_point="ABW",10
-text_file.write('{},{}\n'.format(cifid,cluster_point))
-text_file.close()
-sys.exit()
 cifadress_list=pd.read_csv('result/allzeorite/picupadress',index_col=0).cifadress.to_list()
 import os
-for cifadress in cifadress_list:
-  cifid=os.path.basename
+for i,cifadress in enumerate(cifadress_list):
+  cifid=os.path.basename(cifadress)
+  print('cif {}/{}'.format(i+1,len(cifadress_list)))
   print(cifid)
-  d.cad_crystal_point(cifadress)
-  text_file=open(resulttxt,'w')
-  text_file.write(cifid,d.cluster_point)
+  d.cal_crystal_point(cifadress)
+  text_file=open(resulttxt,'a')
+  text_file.write('{},{}\n'.format(cifid,d.crystal_point))
   text_file.close()
