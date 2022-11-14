8ea36d1c15a7f966b2a0117ac643a6415c8a1c3f
Kazukifujii <fuziikazuki@gmail.com>
2022-11-14 14:53:52 +0900

    reduse dataset

--- remakedataset.py
+++ remakedataset.py
@@ -59,3 +59,23 @@ for i,data in ciflistdf.iterrows():
             cluster.rotation(pattern=pattern)
             cluster.cluster_coords.to_csv('{}_{}_{}.csv'.format(cifid,isite,pattern))
     os.chdir(cwd)
+
+
+#reduce dataset
+from crystal_emd.cluster_adress_func import fcluster_list
+resultdir='database'
+orignal_dataset='cluster_dataset'
+fclusterdf=fcluster_list(orignal_dataset)
+import shutil,os
+
+if os.path.isdir(resultdir):
+    shutil.rmtree(resultdir)
+
+os.mkdir(resultdir)
+for i,data in fclusterdf.iterrows():
+    for pattern in range(12):
+        clusteradress='{}/{}_{}_{}.csv'.format(data.adress,data.cifid,data.isite,pattern)
+        if not os.path.isfile(clusteradress):
+            print('not file ',clusteradress)
+        copyadress='{}/{}_{}_{}.csv'.format(resultdir,data.cifid,data.isite,pattern)
+        shutil.copy(clusteradress,copyadress)
