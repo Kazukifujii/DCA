ed15ccee2b000f7162083ca55ec868213a717b6c
Kazukifujii <fuziikazuki@gmail.com>
2023-06-28 13:18:07 +0900

    add make_clustring_func

--- Distance_based_on_Cluster_Analysis/clustering_func.py
+++ Distance_based_on_Cluster_Analysis/clustering_func.py
@@ -50,3 +50,31 @@ def make_clusering(csvadress,csvn='sort_distance.csv',pngn='cluster.png',method=
     plt.close()
     result_=[(idx[i],num) for i,num in enumerate(list(fcluster(result,fclusternum)))]
     return pd.DataFrame(result_,columns=['isite','fclusternum'])
+
+
+
+import pandas as pd
+from scipy.cluster.hierarchy import linkage, fcluster
+
+def make_inputdf_linkage(cluster_distance_df):
+    grouped_df = cluster_distance_df.groupby(['cifid_i', 'cifid_j', 'isite_i', 'isite_j', 'pattern_i']).min()
+    grouped_df = grouped_df.reset_index().sort_index(axis=1)
+    grouped_df['id_set'] = grouped_df.apply(lambda x: sorted([f"{x['cifid_i']}_{x['isite_i']}", f"{x['cifid_j']}_{x['isite_j']}"]), axis=1)
+    grouped_df = grouped_df.sort_values('id_set').reset_index(drop=True)
+    id_set = grouped_df['id_set'].copy()
+    grouped_df.drop('id_set', axis=1, inplace=True)
+    return grouped_df, id_set
+
+def make_clustering(cluster_distance_df, method='ward', fclusternum=2):
+    sorted_distance_df, id_set = make_inputdf_linkage(cluster_distance_df)
+    result = linkage(sorted_distance_df['distance'].values, method=method)
+    
+    # クラスタリングの結果をまとめる
+    set_list = list(id_set.apply(lambda x: x[0]).unique())
+    set_list.append(id_set.iloc[-1][-1])
+    set_df = pd.DataFrame(set_list, columns=['tag'])
+    set_df[['cifid', 'isite']] = set_df['tag'].str.split('_', expand=True)
+    set_df.drop('tag', axis=1, inplace=True)
+    
+    result_ = [num for num in list(fcluster(result, fclusternum))]
+    return pd.concat([set_df, pd.Series(result_, name='class')], axis=1)
