a08fd284fae98661a3640127573c7a4de66004e6
Kazukifujii <fuziikazuki@gmail.com>
2023-09-07 17:24:58 +0900

    update characterization_DCA.py

--- characterization_DCA.py
+++ characterization_DCA.py
@@ -22,6 +22,21 @@ def parallen_make_cluster_dataset(data,adjacent_num=2):
       make_cluster_dataset(cifid=data.cifid,nn_data_address=nn_data_address,adjacent_num=adjacent_num,rotation=False,outdir=data.cifaddress)
       return 
   
+
+def parallel_calculate_feature(data):
+    #特徴量を計算し、各ディレクトリに結果を保存
+    feature = characteriz_func.calculate_features(data.cifaddress)
+    #特徴量の保存
+    resulttxt=f'{data.cifaddress}/cifpoint'
+    cifid=os.path.basename(data.cifaddress)
+    text_file=open(resulttxt,'w')
+    text_file.write('cifid,point\n')
+    text_file.write('{},{}\n'.format(cifid,feature))
+    text_file.close()
+    #logの保存
+    for j,log in enumerate(characteriz_func.calculate_log):
+      pickle.dump(log,open(f'{data.cifaddress}/log/{cifid}_{j}.pickle','wb'))
+    return 
   
 
 
@@ -38,38 +53,15 @@ def main():
 
   #隣接情報からクラスターを生成
   picdata=make_sort_ciffile('result/{}'.format(cifdir),estimecont='all')
-  total_cluster_num=picdata.Si_len.sum()
-
-  Parallel(n_jobs=-1)([delayed(parallen_make_cluster_dataset)(data,adjacent_num) for i,data in picdata.iterrows()])
-
-  #計算結果の保存
-  
-  resulttxt='result/{}/cifpoint'.format(cifdir)
-  text_file=open(resulttxt,'w')
-  text_file.write('cifid,point\n')
-  text_file.close()
-  if os.path.isdir(f'result/{cifdir}/log'):
-    shutil.rmtree(f'result/{cifdir}/log')
-  os.mkdir(f'result/{cifdir}/log')
+  Parallel(n_jobs=-1)([delayed(parallen_make_cluster_dataset)(data,adjacent_num) for _,data in picdata.iterrows()])
 
   #各cifファイルの特徴量を計算
   characteriz_func=CrystalFeatureCalculator(databaseaddress)
-
   
-  bar = tqdm(total = total_cluster_num)
-  bar.set_description('Progress rate')
-  for i in range(len(picdata)):
-    data = picdata.iloc[i,:]
-    cifid=os.path.basename(data.cifaddress)
-    feature = characteriz_func.calculate_features(data.cifaddress)
-    bar.update(len(glob.glob(os.path.join(data.cifaddress,'*_0.csv'))))
-    #特徴量の保存
-    text_file=open(resulttxt,'a')
-    text_file.write('{},{}\n'.format(cifid,feature))
-    text_file.close()
-    #計算ログの保存
-    
-    for j,log in enumerate(characteriz_func.calculate_log):
-      pickle.dump(log,open(f'result/{cifdir}/log/{cifid}_{j}.pickle','wb'))
+  
+
+  #特徴量の計算
+  Parallel(n_jobs=-1)([delayed(parallel_calculate_feature)(data,characteriz_func) for _,data in picdata.iterrows()])
+
 if __name__=='__main__':
   main()
