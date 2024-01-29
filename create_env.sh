be326e1ba2c1cac19c7d69416d9058032a7f20b2
Kazukifujii <fuziikazuki@gmail.com>
2024-01-29 16:44:28 +0900

    fix create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -3,14 +3,23 @@
 # Set the directory path
 directory="Distance_based_on_Cluster_Analysis/fortran"
 
-# Compile all Fortran files in the directory
-for file in $directory/*.f90; do
-    if [[ -f $file ]]; then
-        echo "Compiling $file..."
-        gfortran -o "make_nnlist.out" "$file"  # Change the output file name
-        echo "Compilation completed."
-    fi
+# Concatenate the file paths
+file_paths=""
+for file in "$directory"/*.f90; do
+    echo "File paths: $file"
+    file_paths="$file_paths $file"
 done
+echo "File paths: $file_paths"
+# Compile all Fortran files
+gfortran $file_paths -o "make_nnlist.out"
+
+
+
+
+# Wait for all background processes to finish
+wait
+
+echo "All compilations completed."
 
 # Move make_nnlist.out to ~/bin
-mv make_nnlist.out ~/bin/
+mv make_nnlist.out ~/.local/bin/make_nnlist.out
